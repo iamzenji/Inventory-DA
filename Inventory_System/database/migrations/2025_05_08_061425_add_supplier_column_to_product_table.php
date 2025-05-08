b71bb77a56cde728d5375e9f853192e9cbe2f3f4
Daryl Huynh De Leon <darylhuynhdeleon@gmail.com>
2025-05-08 15:05:35 +0800

    foreign key

---
+++ Inventory_System/database/migrations/2025_05_08_061425_add_supplier_column_to_product_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('products', function (Blueprint $table) {
+            $table->tinyInteger('supplier_id')->nullable()->after('end_user');
+            $table->foreign('supplier_id')->references('id')->on('suppliers')->onDelete('restrict');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('products', function (Blueprint $table) {
+            $table->dropForeign(['supplier_id']);
+            $table->dropColumn('supplier_id');
+        });
+    }
+};
